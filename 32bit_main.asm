048e6f4c6c6fda3505562714c5e3c1ea6d902a9b
icxd <icoding666@gmail.com>
2023-10-09 08:57:10 +0200

    bootloader fucked

--- 32bit_main.asm
+++ 32bit_main.asm
@@ -1,12 +1,12 @@
 [org 0x7c00] ; bootloader offset
-	mov bp, 0x9000 ; set the stack
-	mov sp, bp
+    mov bp, 0x9000 ; set the stack
+    mov sp, bp
 
-	mov bx, MSG_REAL_MODE
-	call print ; this will be written after the BIOS messages
+    mov bx, MSG_REAL_MODE
+    call print ; This will be written after the BIOS messages
 
-	call switch_to_pm
-	jmp $ ; this will actually never be executed
+    call switch_to_pm
+    jmp $ ; this will actually never be executed
 
 %include "boot_sect_print.asm"
 %include "32bit_gdt.asm"
@@ -14,10 +14,11 @@
 %include "32bit_switch.asm"
 
 [bits 32]
-BEGIN_PM: ; after the switch we wil get here
-	mov ebx, MSG_PROT_MODE
-	call print_string_pm ; note that this will be written at the top left corner
-	jmp $
+BEGIN_PM: ; after the switch we will get here
+    mov ebx, MSG_PROT_MODE
+    call print_string_pm ; Note that this will be written at the top left corner
+
+    jmp $
 
 MSG_REAL_MODE db "Started in 16-bit real mode", 0
 MSG_PROT_MODE db "Loaded 32-bit protected mode", 0
--- 32bit_print.asm
+++ 32bit_print.asm
@@ -5,23 +5,22 @@ VIDEO_MEMORY equ 0xb8000
 WHITE_ON_BLACK equ 0x0f ; the color byte for each character
 
 print_string_pm:
-	pusha
-	mov edx, VIDEO_MEMORY
+    pusha
+    mov edx, VIDEO_MEMORY
+    mov ah, WHITE_ON_BLACK
 
 print_string_pm_loop:
-	mov al, [ebx] ; [ebx] is the address of our character
-	mov ah, WHITE_ON_BLACK
+    mov al, [ebx] ; [ebx] is the address of our character
 
-	cmp al, 0 ; check if end of string
-	je print_string_pm_done
+    cmp al, 0 ; check if end of string
+    je print_string_pm_done
 
-	mov [edx], ax ; store charafcter + attribute in video memory
-	add ebx, 1 ; next char
-	add edx, 2 ; next video memory position
+    mov [edx], ax ; store character + attribute in video memory
+    add ebx, 1 ; next char
+    add edx, 2 ; next video memory position
 
-	jmp print_string_pm_loop
+    jmp print_string_pm_loop
 
 print_string_pm_done:
-	popa
-	ret
-
+    popa
+    ret
